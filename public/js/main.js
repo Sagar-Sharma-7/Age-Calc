c51dc3221d276e9fd81082197292a90984e580ba
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-11-05 09:51:50 +0530

    text content javascript

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+const heading = document.querySelector("#heading");
+const sub_heading = document.querySelector("#sub_heading");
+const dob = document.querySelector("date");
+const cal = document.querySelector(".calculate");
+
+
+// ttext content
+heading.innerHTML = "Age Calculator";
+sub_heading.innerHTML = "Get your age in different units";
+cal.innerHTML = "Calculate";
+
+
