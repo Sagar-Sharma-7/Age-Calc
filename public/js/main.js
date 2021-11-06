2a58a71a4f3d1a8d66ac2f56220f69e042ae210c
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-11-06 09:11:36 +0530

    algo of age in diff units

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,39 @@
 const heading = document.querySelector("#heading");
 const sub_heading = document.querySelector("#sub_heading");
-const dob = document.querySelector("date");
+const dob = document.querySelector("#date");
 const cal = document.querySelector(".calculate");
+const github = document.querySelector("#github");
 
 
 // ttext content
 heading.innerHTML = "Age Calculator";
 sub_heading.innerHTML = "Get your age in different units";
 cal.innerHTML = "Calculate";
+github.innerHTML = "Contribute to my Github";
 
+// functions
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
 
+cal.addEventListener("click", () => {
+    const userDOB = new Date(dob.value);
+    const birthDay = userDOB.getDate();
+    const birthMonth = userDOB.getMonth() + 1;    
+    const birthYear = userDOB.getFullYear();
+    console.table([userDOB, birthDay, birthMonth, birthYear]);
+
+    const leapYear = Math.floor((year - birthYear) / 4);
+    const yearDays = (((year - birthYear) * 365) + leapYear);
+    const monthDays = ((month - birthMonth) * 31) - parseInt(Math.floor(month - birthMonth) / 2);
+    const dateDays = day - birthDay;
+
+    const ageInDays = yearDays + monthDays + dateDays;
+    console.log(ageInDays);
+
+    const ageInHours = ageInDays * 24;
+    const ageInMin = ageInHours * 60;
+    const AgeInSec = ageInMin * 60;
+    
+});
