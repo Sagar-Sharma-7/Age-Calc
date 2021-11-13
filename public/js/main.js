5a6f18e2757d5aa3dcaf1e9a0f6097658335abc0
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-11-13 21:55:58 +0530

    diff units js

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,14 @@ const dob = document.querySelector("#date");
 const cal = document.querySelector(".calculate");
 const github = document.querySelector("#github");
 
+// result span
+const month_span = document.querySelector("#months");
+const week_span = document.querySelector("#weeks");
+const day_span = document.querySelector("#days");
+const hour_span = document.querySelector("#hours");
+const min_span = document.querySelector("#min");
+const sec_span = document.querySelector("#sec"); 
+
 
 // ttext content
 heading.innerHTML = "Age Calculator";
@@ -31,7 +39,7 @@ cal.addEventListener("click", () => {
 
     const ageInMonths = ((year - birthYear) * 12) + (month - birthMonth);
     const ageInDays = yearDays + monthDays + dateDays;
-    const ageInWeeks = ageInDays / 7;
+    const ageInWeeks = Math.floor(ageInDays / 7);
     const ageInHours = ageInDays * 24;
     const ageInMin = ageInHours * 60;
     const AgeInSec = ageInMin * 60;
@@ -42,7 +50,12 @@ cal.addEventListener("click", () => {
     }else if(ageInDays < 0){
         console.log("Incorrect Date of Birth");
     }else{
-        console.log(ageInDays);
+        month_span.innerHTML = ageInMonths;
+        week_span.innerHTML = ageInWeeks;
+        day_span.innerHTML = ageInDays;
+        hour_span.innerHTML = ageInHours;
+        min_span.innerHTML = ageInMin;
+        sec_span.innerHTML = AgeInSec;
     }
 
 });
